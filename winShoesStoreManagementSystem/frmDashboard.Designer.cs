74b126e2f4b3cd7eebf3332fd5aadbe226e7e673
Juniorawan06 <haseebawan.1560@gmail.com>
2023-05-09 18:56:20 +0500

    dashboard updated

--- winShoesStoreManagementSystem/frmDashboard.Designer.cs
+++ winShoesStoreManagementSystem/frmDashboard.Designer.cs
@@ -29,29 +29,29 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnShoesSizes = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btnShoesSizes);
             this.groupBox1.Location = new System.Drawing.Point(1, 0);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(200, 450);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
-            // button1
+            // btnShoesSizes
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.button1.Location = new System.Drawing.Point(0, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(200, 43);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnShoesSizes.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnShoesSizes.Location = new System.Drawing.Point(0, 2);
+            this.btnShoesSizes.Name = "btnShoesSizes";
+            this.btnShoesSizes.Size = new System.Drawing.Size(200, 43);
+            this.btnShoesSizes.TabIndex = 0;
+            this.btnShoesSizes.Text = "Shoes Sizes";
+            this.btnShoesSizes.UseVisualStyleBackColor = false;
             // 
             // frmDashboard
             // 
@@ -69,6 +69,6 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnShoesSizes;
     }
 }
