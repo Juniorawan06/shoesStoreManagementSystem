7efab45ed4da0c62a67fd7c19bdaa1717c20a007
muhammaduzair19 <muhammaduzairilyas@gmail.com>
2023-05-12 12:05:30 -0700

    screen sizes/styles/colors add

---
+++ winShoesStoreManagementSystem/frmShoesColors.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace winShoesStoreManagementSystem
+{
+    partial class frmShoesColors
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.txtShoeSize = new System.Windows.Forms.TextBox();
+            this.lblEnterShoesColor = new System.Windows.Forms.Label();
+            this.lblShoesColors = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnNext
+            // 
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnNext.Location = new System.Drawing.Point(725, 526);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(104, 33);
+            this.btnNext.TabIndex = 14;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnBack.Location = new System.Drawing.Point(601, 526);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(104, 33);
+            this.btnBack.TabIndex = 13;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // txtShoeSize
+            // 
+            this.txtShoeSize.Location = new System.Drawing.Point(458, 270);
+            this.txtShoeSize.Multiline = true;
+            this.txtShoeSize.Name = "txtShoeSize";
+            this.txtShoeSize.Size = new System.Drawing.Size(146, 28);
+            this.txtShoeSize.TabIndex = 12;
+            // 
+            // lblEnterShoesColor
+            // 
+            this.lblEnterShoesColor.AutoSize = true;
+            this.lblEnterShoesColor.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblEnterShoesColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold);
+            this.lblEnterShoesColor.Location = new System.Drawing.Point(254, 270);
+            this.lblEnterShoesColor.Name = "lblEnterShoesColor";
+            this.lblEnterShoesColor.Size = new System.Drawing.Size(181, 24);
+            this.lblEnterShoesColor.TabIndex = 11;
+            this.lblEnterShoesColor.Text = "Enter Shoes Color";
+            // 
+            // lblShoesColors
+            // 
+            this.lblShoesColors.AutoSize = true;
+            this.lblShoesColors.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblShoesColors.Location = new System.Drawing.Point(327, 146);
+            this.lblShoesColors.Name = "lblShoesColors";
+            this.lblShoesColors.Size = new System.Drawing.Size(207, 36);
+            this.lblShoesColors.TabIndex = 10;
+            this.lblShoesColors.Text = "Shoes Colors";
+            // 
+            // frmShoesColors
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 571);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.txtShoeSize);
+            this.Controls.Add(this.lblEnterShoesColor);
+            this.Controls.Add(this.lblShoesColors);
+            this.Name = "frmShoesColors";
+            this.Text = "frmShoesColors";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.TextBox txtShoeSize;
+        private System.Windows.Forms.Label lblEnterShoesColor;
+        private System.Windows.Forms.Label lblShoesColors;
+    }
+}
--- winShoesStoreManagementSystem/frmShoesSizes.Designer.cs
+++ winShoesStoreManagementSystem/frmShoesSizes.Designer.cs
@@ -28,25 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblShoesSizes = new System.Windows.Forms.Label();
+            this.lblEnterShoesSize = new System.Windows.Forms.Label();
+            this.txtShoeSize = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // lblShoesSizes
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(415, 84);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(192, 36);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Shoes Sizes";
+            this.lblShoesSizes.AutoSize = true;
+            this.lblShoesSizes.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblShoesSizes.Location = new System.Drawing.Point(327, 146);
+            this.lblShoesSizes.Name = "lblShoesSizes";
+            this.lblShoesSizes.Size = new System.Drawing.Size(192, 36);
+            this.lblShoesSizes.TabIndex = 5;
+            this.lblShoesSizes.Text = "Shoes Sizes";
+            // 
+            // lblEnterShoesSize
+            // 
+            this.lblEnterShoesSize.AutoSize = true;
+            this.lblEnterShoesSize.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblEnterShoesSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold);
+            this.lblEnterShoesSize.Location = new System.Drawing.Point(254, 270);
+            this.lblEnterShoesSize.Name = "lblEnterShoesSize";
+            this.lblEnterShoesSize.Size = new System.Drawing.Size(171, 24);
+            this.lblEnterShoesSize.TabIndex = 6;
+            this.lblEnterShoesSize.Text = "Enter Shoes Size";
+            // 
+            // txtShoeSize
+            // 
+            this.txtShoeSize.Location = new System.Drawing.Point(458, 270);
+            this.txtShoeSize.Multiline = true;
+            this.txtShoeSize.Name = "txtShoeSize";
+            this.txtShoeSize.Size = new System.Drawing.Size(146, 28);
+            this.txtShoeSize.TabIndex = 7;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnCancel.Location = new System.Drawing.Point(601, 526);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(104, 33);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnNext.Location = new System.Drawing.Point(725, 526);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(104, 33);
+            this.btnNext.TabIndex = 9;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
             // 
             // frmShoesSizes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1037, 552);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(853, 571);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.txtShoeSize);
+            this.Controls.Add(this.lblEnterShoesSize);
+            this.Controls.Add(this.lblShoesSizes);
             this.Name = "frmShoesSizes";
             this.Text = "Shoes Sizes";
             this.ResumeLayout(false);
@@ -56,6 +103,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblShoesSizes;
+        private System.Windows.Forms.Label lblEnterShoesSize;
+        private System.Windows.Forms.TextBox txtShoeSize;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnNext;
     }
 }
