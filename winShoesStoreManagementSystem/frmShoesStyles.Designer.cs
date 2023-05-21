efa6e18ac6faea62ecf6946c254b43f6ee7ebb3f
Juniorawan06 <haseebawan.1560@gmail.com>
2023-05-22 01:44:31 +0500

    updations

--- winShoesStoreManagementSystem/frmShoesStyles.Designer.cs
+++ winShoesStoreManagementSystem/frmShoesStyles.Designer.cs
@@ -28,119 +28,185 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtShoeSize = new System.Windows.Forms.TextBox();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.lblShoesStyles = new System.Windows.Forms.Label();
-            this.gvShoesStyles = new System.Windows.Forms.DataGridView();
-            this.gunaBtnAddOne = new Guna.UI.WinForms.GunaButton();
-            this.gunaBtnAddBulk = new Guna.UI.WinForms.GunaButton();
-            ((System.ComponentModel.ISupportInitialize)(this.gvShoesStyles)).BeginInit();
+            this.txtSearch = new Guna.UI.WinForms.GunaTextBox();
+            this.gvShoesColors = new Guna.UI.WinForms.GunaDataGridView();
+            this.btnAddBulk = new Guna.UI.WinForms.GunaButton();
+            this.btnAddItems = new Guna.UI.WinForms.GunaButton();
+            ((System.ComponentModel.ISupportInitialize)(this.gvShoesColors)).BeginInit();
             this.SuspendLayout();
-            // 
-            // txtShoeSize
-            // 
-            this.txtShoeSize.Location = new System.Drawing.Point(543, 171);
-            this.txtShoeSize.Multiline = true;
-            this.txtShoeSize.Name = "txtShoeSize";
-            this.txtShoeSize.Size = new System.Drawing.Size(225, 34);
-            this.txtShoeSize.TabIndex = 17;
             // 
             // lblShoesStyles
             // 
             this.lblShoesStyles.AutoSize = true;
-            this.lblShoesStyles.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblShoesStyles.Location = new System.Drawing.Point(324, 109);
+            this.lblShoesStyles.Font = new System.Drawing.Font("Segoe UI Black", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.lblShoesStyles.Location = new System.Drawing.Point(39, 14);
             this.lblShoesStyles.Name = "lblShoesStyles";
-            this.lblShoesStyles.Size = new System.Drawing.Size(201, 36);
+            this.lblShoesStyles.Size = new System.Drawing.Size(195, 41);
             this.lblShoesStyles.TabIndex = 15;
             this.lblShoesStyles.Text = "Shoes Styles";
             // 
-            // gvShoesStyles
+            // txtSearch
             // 
-            this.gvShoesStyles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gvShoesStyles.Location = new System.Drawing.Point(78, 228);
-            this.gvShoesStyles.Name = "gvShoesStyles";
-            this.gvShoesStyles.RowHeadersWidth = 51;
-            this.gvShoesStyles.RowTemplate.Height = 24;
-            this.gvShoesStyles.Size = new System.Drawing.Size(690, 59);
-            this.gvShoesStyles.TabIndex = 20;
+            this.txtSearch.BackColor = System.Drawing.Color.Transparent;
+            this.txtSearch.BaseColor = System.Drawing.Color.White;
+            this.txtSearch.BorderColor = System.Drawing.Color.Silver;
+            this.txtSearch.BorderSize = 1;
+            this.txtSearch.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtSearch.FocusedBaseColor = System.Drawing.Color.White;
+            this.txtSearch.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.txtSearch.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtSearch.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtSearch.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.txtSearch.Location = new System.Drawing.Point(576, 116);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.PasswordChar = '\0';
+            this.txtSearch.Radius = 8;
+            this.txtSearch.SelectedText = "";
+            this.txtSearch.Size = new System.Drawing.Size(223, 33);
+            this.txtSearch.TabIndex = 33;
+            this.txtSearch.Text = "Search...";
             // 
-            // gunaBtnAddOne
+            // gvShoesColors
             // 
-            this.gunaBtnAddOne.AnimationHoverSpeed = 0.07F;
-            this.gunaBtnAddOne.AnimationSpeed = 0.03F;
-            this.gunaBtnAddOne.BackColor = System.Drawing.Color.Transparent;
-            this.gunaBtnAddOne.BaseColor = System.Drawing.Color.White;
-            this.gunaBtnAddOne.BorderColor = System.Drawing.Color.Black;
-            this.gunaBtnAddOne.BorderSize = 1;
-            this.gunaBtnAddOne.DialogResult = System.Windows.Forms.DialogResult.None;
-            this.gunaBtnAddOne.FocusedColor = System.Drawing.Color.Empty;
-            this.gunaBtnAddOne.Font = new System.Drawing.Font("Segoe UI Black", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gunaBtnAddOne.ForeColor = System.Drawing.Color.Black;
-            this.gunaBtnAddOne.Image = null;
-            this.gunaBtnAddOne.ImageSize = new System.Drawing.Size(20, 20);
-            this.gunaBtnAddOne.Location = new System.Drawing.Point(567, 12);
-            this.gunaBtnAddOne.Name = "gunaBtnAddOne";
-            this.gunaBtnAddOne.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
-            this.gunaBtnAddOne.OnHoverBorderColor = System.Drawing.Color.Black;
-            this.gunaBtnAddOne.OnHoverForeColor = System.Drawing.Color.White;
-            this.gunaBtnAddOne.OnHoverImage = null;
-            this.gunaBtnAddOne.OnPressedColor = System.Drawing.SystemColors.ActiveBorder;
-            this.gunaBtnAddOne.Radius = 15;
-            this.gunaBtnAddOne.Size = new System.Drawing.Size(124, 33);
-            this.gunaBtnAddOne.TabIndex = 21;
-            this.gunaBtnAddOne.Text = "Add One";
-            this.gunaBtnAddOne.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.gvShoesColors.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.gvShoesColors.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gvShoesColors.BackgroundColor = System.Drawing.Color.White;
+            this.gvShoesColors.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.gvShoesColors.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.gvShoesColors.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gvShoesColors.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.gvShoesColors.ColumnHeadersHeight = 4;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.gvShoesColors.DefaultCellStyle = dataGridViewCellStyle3;
+            this.gvShoesColors.EnableHeadersVisualStyles = false;
+            this.gvShoesColors.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.gvShoesColors.Location = new System.Drawing.Point(46, 163);
+            this.gvShoesColors.Margin = new System.Windows.Forms.Padding(4);
+            this.gvShoesColors.Name = "gvShoesColors";
+            this.gvShoesColors.RowHeadersVisible = false;
+            this.gvShoesColors.RowHeadersWidth = 51;
+            this.gvShoesColors.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gvShoesColors.Size = new System.Drawing.Size(753, 320);
+            this.gvShoesColors.TabIndex = 32;
+            this.gvShoesColors.Theme = Guna.UI.WinForms.GunaDataGridViewPresetThemes.Guna;
+            this.gvShoesColors.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.gvShoesColors.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.gvShoesColors.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.gvShoesColors.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.gvShoesColors.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.gvShoesColors.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.gvShoesColors.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.gvShoesColors.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gvShoesColors.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.gvShoesColors.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            this.gvShoesColors.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.gvShoesColors.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.gvShoesColors.ThemeStyle.HeaderStyle.Height = 4;
+            this.gvShoesColors.ThemeStyle.ReadOnly = false;
+            this.gvShoesColors.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.gvShoesColors.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.gvShoesColors.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            this.gvShoesColors.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.gvShoesColors.ThemeStyle.RowsStyle.Height = 22;
+            this.gvShoesColors.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.gvShoesColors.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
             // 
-            // gunaBtnAddBulk
+            // btnAddBulk
             // 
-            this.gunaBtnAddBulk.AnimationHoverSpeed = 0.07F;
-            this.gunaBtnAddBulk.AnimationSpeed = 0.03F;
-            this.gunaBtnAddBulk.BackColor = System.Drawing.Color.Transparent;
-            this.gunaBtnAddBulk.BaseColor = System.Drawing.Color.White;
-            this.gunaBtnAddBulk.BorderColor = System.Drawing.Color.Black;
-            this.gunaBtnAddBulk.BorderSize = 1;
-            this.gunaBtnAddBulk.DialogResult = System.Windows.Forms.DialogResult.None;
-            this.gunaBtnAddBulk.FocusedColor = System.Drawing.Color.Empty;
-            this.gunaBtnAddBulk.Font = new System.Drawing.Font("Segoe UI Black", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gunaBtnAddBulk.ForeColor = System.Drawing.Color.Black;
-            this.gunaBtnAddBulk.Image = null;
-            this.gunaBtnAddBulk.ImageSize = new System.Drawing.Size(20, 20);
-            this.gunaBtnAddBulk.Location = new System.Drawing.Point(717, 12);
-            this.gunaBtnAddBulk.Name = "gunaBtnAddBulk";
-            this.gunaBtnAddBulk.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
-            this.gunaBtnAddBulk.OnHoverBorderColor = System.Drawing.Color.Black;
-            this.gunaBtnAddBulk.OnHoverForeColor = System.Drawing.Color.White;
-            this.gunaBtnAddBulk.OnHoverImage = null;
-            this.gunaBtnAddBulk.OnPressedColor = System.Drawing.SystemColors.ActiveBorder;
-            this.gunaBtnAddBulk.Radius = 15;
-            this.gunaBtnAddBulk.Size = new System.Drawing.Size(124, 33);
-            this.gunaBtnAddBulk.TabIndex = 22;
-            this.gunaBtnAddBulk.Text = "Add Bulk";
-            this.gunaBtnAddBulk.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.btnAddBulk.AnimationHoverSpeed = 0.07F;
+            this.btnAddBulk.AnimationSpeed = 0.03F;
+            this.btnAddBulk.BackColor = System.Drawing.Color.Transparent;
+            this.btnAddBulk.BaseColor = System.Drawing.Color.White;
+            this.btnAddBulk.BorderColor = System.Drawing.Color.Black;
+            this.btnAddBulk.BorderSize = 1;
+            this.btnAddBulk.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnAddBulk.FocusedColor = System.Drawing.Color.Empty;
+            this.btnAddBulk.Font = new System.Drawing.Font("Segoe UI Black", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddBulk.ForeColor = System.Drawing.Color.Black;
+            this.btnAddBulk.Image = null;
+            this.btnAddBulk.ImageSize = new System.Drawing.Size(20, 20);
+            this.btnAddBulk.Location = new System.Drawing.Point(654, 22);
+            this.btnAddBulk.Name = "btnAddBulk";
+            this.btnAddBulk.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.btnAddBulk.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.btnAddBulk.OnHoverForeColor = System.Drawing.Color.White;
+            this.btnAddBulk.OnHoverImage = null;
+            this.btnAddBulk.OnPressedColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnAddBulk.Radius = 5;
+            this.btnAddBulk.Size = new System.Drawing.Size(148, 33);
+            this.btnAddBulk.TabIndex = 31;
+            this.btnAddBulk.Text = "+ Add Bulk";
+            this.btnAddBulk.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnAddItems
+            // 
+            this.btnAddItems.AnimationHoverSpeed = 0.07F;
+            this.btnAddItems.AnimationSpeed = 0.03F;
+            this.btnAddItems.BackColor = System.Drawing.Color.Transparent;
+            this.btnAddItems.BaseColor = System.Drawing.Color.White;
+            this.btnAddItems.BorderColor = System.Drawing.Color.Black;
+            this.btnAddItems.BorderSize = 1;
+            this.btnAddItems.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnAddItems.FocusedColor = System.Drawing.Color.Empty;
+            this.btnAddItems.Font = new System.Drawing.Font("Segoe UI Black", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddItems.ForeColor = System.Drawing.Color.Black;
+            this.btnAddItems.Image = null;
+            this.btnAddItems.ImageSize = new System.Drawing.Size(20, 20);
+            this.btnAddItems.Location = new System.Drawing.Point(504, 22);
+            this.btnAddItems.Name = "btnAddItems";
+            this.btnAddItems.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.btnAddItems.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.btnAddItems.OnHoverForeColor = System.Drawing.Color.White;
+            this.btnAddItems.OnHoverImage = null;
+            this.btnAddItems.OnPressedColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnAddItems.Radius = 5;
+            this.btnAddItems.Size = new System.Drawing.Size(144, 33);
+            this.btnAddItems.TabIndex = 30;
+            this.btnAddItems.Text = "+ Add Items";
+            this.btnAddItems.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // frmShoesStyles
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(853, 571);
-            this.Controls.Add(this.gunaBtnAddBulk);
-            this.Controls.Add(this.gunaBtnAddOne);
-            this.Controls.Add(this.gvShoesStyles);
-            this.Controls.Add(this.txtShoeSize);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.gvShoesColors);
+            this.Controls.Add(this.btnAddBulk);
+            this.Controls.Add(this.btnAddItems);
             this.Controls.Add(this.lblShoesStyles);
             this.Name = "frmShoesStyles";
             this.Text = "Shoes Styles";
-            ((System.ComponentModel.ISupportInitialize)(this.gvShoesStyles)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvShoesColors)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.TextBox txtShoeSize;
         private System.Windows.Forms.Label lblShoesStyles;
-        private System.Windows.Forms.DataGridView gvShoesStyles;
-        private Guna.UI.WinForms.GunaButton gunaBtnAddOne;
-        private Guna.UI.WinForms.GunaButton gunaBtnAddBulk;
+        private Guna.UI.WinForms.GunaTextBox txtSearch;
+        private Guna.UI.WinForms.GunaDataGridView gvShoesColors;
+        private Guna.UI.WinForms.GunaButton btnAddBulk;
+        private Guna.UI.WinForms.GunaButton btnAddItems;
     }
 }
